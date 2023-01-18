d05ac16c71c43def676f6d506b1bba021257e2b3
Wailo0 <iymashev@gmail.com>
2023-01-18 17:04:13 +0300

    c1

--- src/main/java/app/model/Animal.java
+++ src/main/java/app/model/Animal.java
@@ -1,4 +1,8 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
 public abstract class Animal {
+
 }
--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,38 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("singleton")
 public class AnimalsCage {
 
+    private  Timer timer;
+
+
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+
+    public Timer getTimer() {
+        return timer;
+    }
+
+    public AnimalsCage(Timer timer) {
+        this.timer = timer;
+    }
+
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 }
